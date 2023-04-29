d5b82ad2d85e67cfa4a20648780e4b5996ee768d
Yao Weijie <yaowj.it@outlook.com>
2023-04-29 20:43:00 +0800

    rename init.py

--- DLlib/mynn/initialize.py
+++ DLlib/mynn/initialize.py
@@ -1,15 +1,17 @@
 import numpy as np
 
+from .variable import AGarry
 
-def uniform_(arr: np.ndarray, a=0.0, b=1.0):
+
+def uniform_(arr: AGarry, a=0.0, b=1.0):
     tmp_arr = np.random.rand(*arr.shape) * (b - a) + a
     arr.setfield(tmp_arr, dtype=tmp_arr.dtype)
 
 
-def normal_(arr: np.ndarray, mean=0.0, std=1.0):
+def normal_(arr: AGarry, mean=0.0, std=1.0):
     tmp_arr = np.random.randn(*arr.shape) * std + mean
     arr.setfield(tmp_arr, dtype=tmp_arr.dtype)
 
 
-def const_(arr: np.ndarray, val):
+def const_(arr: AGarry, val):
     arr.setfield(val, dtype=np.float)
